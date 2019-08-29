6b878c24afc7fdb18c6a4a7f1c592ce47025dba9
rinor <rinorhoxha@gmail.com>
2019-08-29 20:30:58 +0000

    JCLI: key - golint fix, tests and test data

--- jcli/key.go
+++ jcli/key.go
@@ -5,155 +5,141 @@ import (
 	"io/ioutil"
 )
 
-// KeyGenerate - generate a private key without seed.
-//
-// jcli key generate --type=<key_type> [OUTPUT_FILE]
-func KeyGenerate(
-	key_type string,
-	output_file_sk string,
-) ([]byte, error) {
-	if key_type == "" {
-		return nil, fmt.Errorf("parameter missing : %s", "key_type")
-	}
-
-	return KeyGenerateFromSeed("", key_type, output_file_sk)
-}
-
-// KeyGenerateFromSeed - generate a private key using a SEED value.
+// KeyGenerate - generate a private key using a SEED value.
 //
 // jcli key generate --type=<key_type> [--seed=<SEED>] [OUTPUT_FILE]
-func KeyGenerateFromSeed(
+func KeyGenerate(
 	seed string,
-	key_type string,
-	output_file_sk string,
+	keyType string,
+	outputFileSk string,
 ) ([]byte, error) {
-	if key_type == "" {
-		return nil, fmt.Errorf("parameter missing : %s", "key_type")
+	if keyType == "" {
+		return nil, fmt.Errorf("parameter missing : %s", "keyType")
 	}
-	arg := []string{"key", "generate", "--type", key_type}
+	arg := []string{"key", "generate", "--type", keyType}
 	if seed != "" {
 		arg = append(arg, "--seed", seed)
 	}
-	if output_file_sk != "" {
-		arg = append(arg, output_file_sk)
+	if outputFileSk != "" {
+		arg = append(arg, outputFileSk)
 	}
 
 	out, err := execStd(nil, "jcli", arg...)
-	if err != nil || output_file_sk == "" {
+	if err != nil || outputFileSk == "" {
 		return out, err
 	}
 
-	return ioutil.ReadFile(output_file_sk)
+	return ioutil.ReadFile(outputFileSk)
 }
 
 // KeyToPublic - get the public key out of a given private key.
 //
 // STDIN | jcli key to-public [--input=input_file] [OUTPUT_FILE] (input file has priority over STDIN)
 func KeyToPublic(
-	stdin_sk []byte,
-	input_file_sk string,
-	output_file_pk string,
+	stdinSk []byte,
+	inputFileSk string,
+	outputFilePk string,
 ) ([]byte, error) {
-	if len(stdin_sk) == 0 && input_file_sk == "" {
-		return nil, fmt.Errorf("%s : EMPTY and parameter missing : %s", "stdin_sk", "input_file_sk")
+	if len(stdinSk) == 0 && inputFileSk == "" {
+		return nil, fmt.Errorf("%s : EMPTY and parameter missing : %s", "stdinSk", "inputFileSk")
 	}
 
 	arg := []string{"key", "to-public"}
-	if input_file_sk != "" {
-		arg = append(arg, "--input", input_file_sk)
-		stdin_sk = nil // reset STDIN - not needed since input_file has priority over STDIN
+	if inputFileSk != "" {
+		arg = append(arg, "--input", inputFileSk)
+		stdinSk = nil // reset STDIN - not needed since input_file has priority over STDIN
 	}
-	if output_file_pk != "" {
-		arg = append(arg, output_file_pk) // TODO: UPSTREAM unify with "--output" as other file output commands
+	if outputFilePk != "" {
+		arg = append(arg, outputFilePk) // TODO: UPSTREAM unify with "--output" as other file output commands
 	}
 
-	out, err := execStd(stdin_sk, "jcli", arg...)
-	if err != nil || output_file_pk == "" {
+	out, err := execStd(stdinSk, "jcli", arg...)
+	if err != nil || outputFilePk == "" {
 		return out, err
 	}
 
-	return ioutil.ReadFile(output_file_pk)
+	return ioutil.ReadFile(outputFilePk)
 }
 
 // KeyToBytes - get the bytes out of a private key. [TODO: encodes also public key but corverts it wrong]
 //
 // STDIN | jcli key to-bytes [OUTPUT_FILE] [INPUT_FILE]
 func KeyToBytes(
-	stdin_key []byte,
-	output_file_bytes string,
-	input_file_key string,
+	stdinSk []byte,
+	outputFile string,
+	inputFileSk string,
 ) ([]byte, error) {
-	if len(stdin_key) == 0 && input_file_key == "" {
-		return nil, fmt.Errorf("%s : EMPTY and parameter missing : %s", "stdin_key", "input_file_key")
+	if len(stdinSk) == 0 && inputFileSk == "" {
+		return nil, fmt.Errorf("%s : EMPTY and parameter missing : %s", "stdinSk", "inputFileSk")
 	}
 
 	arg := []string{"key", "to-bytes"}
-	if output_file_bytes != "" {
-		arg = append(arg, output_file_bytes)
+	if outputFile != "" {
+		arg = append(arg, outputFile)
 	}
-	if input_file_key != "" && output_file_bytes != "" {
-		arg = append(arg, input_file_key) // TODO: UPSTREAM unify with "--input" as other file input commands
-		stdin_key = nil                   // reset STDIN since not needed
+	if inputFileSk != "" && outputFile != "" {
+		arg = append(arg, inputFileSk) // TODO: UPSTREAM unify with "--input" as other file input commands
+		stdinSk = nil                  // reset STDIN since not needed
 	}
 
 	// TODO: Remove this once UPSTREAM fixed (--input and --output)
 	// convert input_file to stdin
-	if input_file_key != "" && output_file_bytes == "" {
-		var err error // prevent variable shadowing of stdin_key
-		stdin_key, err = ioutil.ReadFile(input_file_key)
+	if inputFileSk != "" && outputFile == "" {
+		var err error // prevent variable shadowing of stdinSk
+		stdinSk, err = ioutil.ReadFile(inputFileSk)
 		if err != nil {
 			return nil, err
 		}
 	}
 
-	out, err := execStd(stdin_key, "jcli", arg...)
-	if err != nil || output_file_bytes == "" {
+	out, err := execStd(stdinSk, "jcli", arg...)
+	if err != nil || outputFile == "" {
 		return out, err
 	}
 
-	return ioutil.ReadFile(output_file_bytes)
+	return ioutil.ReadFile(outputFile)
 }
 
 // KeyFromBytes - retrive a private key from the given bytes. [TODO: UPSTREAM encodes also public key but corverts it wrong]
 //
 // STDIN | jcli key from-bytes --type=<key_type> [INPUT_BYTES] [OUTPUT_FILE]
 func KeyFromBytes(
-	stdin_key []byte,
-	key_type string,
-	input_file_bytes string,
-	output_file_key string,
+	stdinSk []byte,
+	keyType string,
+	inputFile string,
+	outputFileSk string,
 ) ([]byte, error) {
-	if key_type == "" {
-		return nil, fmt.Errorf("parameter missing : %s", "key_type")
+	if keyType == "" {
+		return nil, fmt.Errorf("parameter missing : %s", "keyType")
 	}
-	if len(stdin_key) == 0 && input_file_bytes == "" {
-		return nil, fmt.Errorf("%s : EMPTY and parameter missing : %s", "stdin_key", "input_file_bytes")
-	}
-
-	arg := []string{"key", "from-bytes", "--type", key_type}
-	if input_file_bytes != "" {
-		arg = append(arg, input_file_bytes) // TODO: UPSTREAM unify with "--input" as other file input commands
-		stdin_key = nil                     // reset STDIN since not needed
-	}
-	if output_file_key != "" && input_file_bytes != "" {
-		arg = append(arg, output_file_key) // TODO: UPSTREAM unify with "--output" as other file output commands
+	if len(stdinSk) == 0 && inputFile == "" {
+		return nil, fmt.Errorf("%s : EMPTY and parameter missing : %s", "stdinSk", "inputFile")
 	}
 
-	out, err := execStd(stdin_key, "jcli", arg...)
-	if err != nil /* || output_file_key == "" */ {
+	arg := []string{"key", "from-bytes", "--type", keyType}
+	if inputFile != "" {
+		arg = append(arg, inputFile) // TODO: UPSTREAM unify with "--input" as other file input commands
+		stdinSk = nil                // reset STDIN since not needed
+	}
+	if outputFileSk != "" && inputFile != "" {
+		arg = append(arg, outputFileSk) // TODO: UPSTREAM unify with "--output" as other file output commands
+	}
+
+	out, err := execStd(stdinSk, "jcli", arg...)
+	if err != nil /* || outputFileSk == "" */ {
 		return out, err
 	}
 
 	// TODO: Remove this once/if UPSTREAM fixed (--input and --output)
 	// convert stdout to output_file
-	if output_file_key != "" && input_file_bytes == "" {
-		if err = ioutil.WriteFile(output_file_key, out, 0644); err != nil {
+	if outputFileSk != "" && inputFile == "" {
+		if err = ioutil.WriteFile(outputFileSk, out, 0644); err != nil {
 			return nil, err
 		}
 	}
-	if output_file_key == "" {
+	if outputFileSk == "" {
 		return out, nil
 	}
 
-	return ioutil.ReadFile(output_file_key)
+	return ioutil.ReadFile(outputFileSk)
 }
---
+++ jcli/key_test.go
@@ -0,0 +1,141 @@
+package jcli_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/rinor/jorcli/jcli"
+)
+
+func ExampleKeyGenerate_noseed() {
+	var (
+		seed         = ""
+		keyType      = "ed25519extended"
+		outputFileSk = ""
+	)
+
+	privateKey, err := jcli.KeyGenerate(seed, keyType, outputFileSk)
+
+	if err != nil {
+		fmt.Printf("KeyGenerate: %s", err)
+	} else {
+		fmt.Printf("%s", string(privateKey))
+	}
+}
+
+func ExampleKeyGenerate_seed() {
+	var (
+		seed         = "0000000000000000000000000000000000000000000000000000000000000000"
+		keyType      = "ed25519extended"
+		outputFileSk = ""
+	)
+
+	privateKey, err := jcli.KeyGenerate(seed, keyType, outputFileSk)
+
+	if err != nil {
+		fmt.Printf("KeyGenerate: %s", err)
+	} else {
+		fmt.Printf("%s", string(privateKey))
+	}
+	// Output:
+	//
+	// ed25519e_sk1wzuwptdq7y7eqszadtj48p4a9z7ayxdc5zx76x4gxmhuezmhp4ra5s2e03g4wjydwujwq0acmp9rw6jrhr6p2x9prnpc0dnfkthxtps9029w4
+}
+
+func TestKeyToPublic_file(t *testing.T) {
+	var (
+		stdinSk           []byte
+		inputFileSk       = filePath(t, "private_key_txt.golden")
+		outputFilePk      = ""
+		expectedPublicKey = loadBytes(t, "public_key_txt.golden")
+	)
+
+	publicKey, err := jcli.KeyToPublic(stdinSk, inputFileSk, outputFilePk)
+	ok(t, err)
+	equals(t, expectedPublicKey, publicKey) // Prod: bytes.Equal(expectedPublicKey, publicKey)
+}
+
+func ExampleKeyToPublic_stdin() {
+	var (
+		stdinSk      = []byte("ed25519e_sk1wzuwptdq7y7eqszadtj48p4a9z7ayxdc5zx76x4gxmhuezmhp4ra5s2e03g4wjydwujwq0acmp9rw6jrhr6p2x9prnpc0dnfkthxtps9029w4")
+		inputFileSk  = ""
+		outputFilePk = ""
+	)
+
+	publicKey, err := jcli.KeyToPublic(stdinSk, inputFileSk, outputFilePk)
+
+	if err != nil {
+		fmt.Printf("KeyToPublic: %s", err)
+	} else {
+		fmt.Printf("%s", string(publicKey))
+	}
+	// Output:
+	//
+	// ed25519_pk10p43s2c5g3hhdklz9k6awwy5nvv7cnkwv6szgaxvac4ju0jm2a0qyf6j8v
+}
+
+func TestKeyToBytes_file(t *testing.T) {
+	var (
+		stdinSk                 []byte
+		outputFile              = ""
+		inputFileSk             = filePath(t, "private_key_txt.golden")
+		expectedPrivateKeyBytes = loadBytes(t, "private_key_bytes.golden")
+	)
+
+	privateKeyBytes, err := jcli.KeyToBytes(stdinSk, outputFile, inputFileSk)
+	ok(t, err)
+	equals(t, expectedPrivateKeyBytes, privateKeyBytes) // Prod: bytes.Equal(expectedPublicKey, actualPublicKey)
+}
+
+func ExampleKeyToBytes_stdin() {
+	var (
+		stdinSk     = []byte("ed25519e_sk1wzuwptdq7y7eqszadtj48p4a9z7ayxdc5zx76x4gxmhuezmhp4ra5s2e03g4wjydwujwq0acmp9rw6jrhr6p2x9prnpc0dnfkthxtps9029w4")
+		outputFile  = ""
+		inputFileSk = ""
+	)
+
+	privateKeyBytes, err := jcli.KeyToBytes(stdinSk, outputFile, inputFileSk)
+
+	if err != nil {
+		fmt.Printf("KeyToBytes: %s", err)
+	} else {
+		fmt.Printf("%s", string(privateKeyBytes))
+	}
+	// Output:
+	//
+	// 70b8e0ada0f13d90405d6ae55386bd28bdd219b8a08ded1aa836efcc8b770d47da41597c5157488d7724e03fb8d84a376a43b8f41518a11cc387b669b2ee6586
+}
+
+func TestKeyFromBytes_file(t *testing.T) {
+	var (
+		stdinSk            []byte
+		keyType            = "ed25519extended"
+		inputFile          = filePath(t, "private_key_bytes.golden")
+		outputFileSk       = ""
+		expectedPrivateKey = loadBytes(t, "private_key_txt.golden")
+	)
+
+	privateKey, err := jcli.KeyFromBytes(stdinSk, keyType, inputFile, outputFileSk)
+	ok(t, err)
+	equals(t, expectedPrivateKey, privateKey) // Prod: bytes.Equal(expectedPublicKey, privateKey)
+}
+
+func ExampleKeyFromBytes_stdin() {
+	var (
+		stdinSk      = []byte("70b8e0ada0f13d90405d6ae55386bd28bdd219b8a08ded1aa836efcc8b770d47da41597c5157488d7724e03fb8d84a376a43b8f41518a11cc387b669b2ee6586")
+		keyType      = "ed25519extended"
+		inputFile    = ""
+		outputFileSk = ""
+	)
+
+	privateKey, err := jcli.KeyFromBytes(stdinSk, keyType, inputFile, outputFileSk)
+
+	if err != nil {
+		fmt.Printf("KeyFromBytes: %s", err)
+	} else {
+		fmt.Printf("%s", string(privateKey))
+	}
+	// Output:
+	//
+	// ed25519e_sk1wzuwptdq7y7eqszadtj48p4a9z7ayxdc5zx76x4gxmhuezmhp4ra5s2e03g4wjydwujwq0acmp9rw6jrhr6p2x9prnpc0dnfkthxtps9029w4
+}
