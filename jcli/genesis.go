70fb3ccd8b206031274050a5889c71ad2a1cd8a7
rinor <rinorhoxha@gmail.com>
2019-08-29 14:15:11 +0000

    JCLI: genesis - golint fix, add tests and test data

--- jcli/genesis.go
+++ jcli/genesis.go
@@ -16,76 +16,76 @@ func GenesisInit() ([]byte, error) {
 //
 // STDIN | jcli genesis hash [--input <FILE_INPUT>]
 func GenesisHash(
-	block0_bin []byte,
-	input_file string,
+	block0Bin []byte,
+	inputFile string,
 ) ([]byte, error) {
-	if len(block0_bin) == 0 && input_file == "" {
-		return nil, fmt.Errorf("%s : EMPTY and parameter missing : %s", "block0_bin", "input_file")
+	if len(block0Bin) == 0 && inputFile == "" {
+		return nil, fmt.Errorf("%s : EMPTY and parameter missing : %s", "block0Bin", "inputFile")
 	}
 
 	arg := []string{"genesis", "hash"}
-	if input_file != "" {
-		arg = append(arg, "--input", input_file)
-		block0_bin = nil // reset STDIN - not needed since input_file has priority over STDIN
+	if inputFile != "" {
+		arg = append(arg, "--input", inputFile)
+		block0Bin = nil // reset STDIN - not needed since input_file has priority over STDIN
 	}
 
-	return execStd(block0_bin, "jcli", arg...)
+	return execStd(block0Bin, "jcli", arg...)
 }
 
 // GenesisEncode - create the block 0 file (the genesis block of the blockchain) from a given yaml file.
 //
 // STDIN | jcli genesis encode [--input <FILE_INPUT>] [--output <FILE_OUTPUT>]
 func GenesisEncode(
-	block0_txt []byte,
-	input_file string,
-	output_file string,
+	block0Txt []byte,
+	inputFile string,
+	outputFile string,
 ) ([]byte, error) {
-	if len(block0_txt) == 0 && input_file == "" {
-		return nil, fmt.Errorf("%s : EMPTY and parameter missing : %s", "block0_txt", "input_file")
+	if len(block0Txt) == 0 && inputFile == "" {
+		return nil, fmt.Errorf("%s : EMPTY and parameter missing : %s", "block0Txt", "inputFile")
 	}
 
 	arg := []string{"genesis", "encode"}
-	if input_file != "" {
-		arg = append(arg, "--input", input_file)
-		block0_txt = nil // reset STDIN - not needed since input_file has priority over STDIN
+	if inputFile != "" {
+		arg = append(arg, "--input", inputFile)
+		block0Txt = nil // reset STDIN - not needed since input_file has priority over STDIN
 	}
-	if output_file != "" {
-		arg = append(arg, "--output", output_file)
+	if outputFile != "" {
+		arg = append(arg, "--output", outputFile)
 	}
 
-	out, err := execStd(block0_txt, "jcli", arg...)
-	if err != nil || output_file == "" {
+	out, err := execStd(block0Txt, "jcli", arg...)
+	if err != nil || outputFile == "" {
 		return out, err
 	}
 
-	return ioutil.ReadFile(output_file)
+	return ioutil.ReadFile(outputFile)
 }
 
 // GenesisDecode - Decode the block 0 and print the corresponding YAML file.
 //
 // STDIN | jcli genesis decode [--input <FILE_INPUT>] [--output <FILE_OUTPUT>]
 func GenesisDecode(
-	block0_bin []byte,
-	input_file string,
-	output_file string,
+	block0Bin []byte,
+	inputFile string,
+	outputFile string,
 ) ([]byte, error) {
-	if len(block0_bin) == 0 && input_file == "" {
-		return nil, fmt.Errorf("%s : EMPTY and parameter missing : %s", "block0_bin", "input_file")
+	if len(block0Bin) == 0 && inputFile == "" {
+		return nil, fmt.Errorf("%s : EMPTY and parameter missing : %s", "block0Bin", "inputFile")
 	}
 
 	arg := []string{"genesis", "decode"}
-	if input_file != "" {
-		arg = append(arg, "--input", input_file)
-		block0_bin = nil // reset STDIN - not needed since input_file has priority over STDIN
+	if inputFile != "" {
+		arg = append(arg, "--input", inputFile)
+		block0Bin = nil // reset STDIN - not needed since input_file has priority over STDIN
 	}
-	if output_file != "" {
-		arg = append(arg, "--output", output_file)
+	if outputFile != "" {
+		arg = append(arg, "--output", outputFile)
 	}
 
-	out, err := execStd(block0_bin, "jcli", arg...)
-	if err != nil || output_file == "" {
+	out, err := execStd(block0Bin, "jcli", arg...)
+	if err != nil || outputFile == "" {
 		return out, err
 	}
 
-	return ioutil.ReadFile(output_file)
+	return ioutil.ReadFile(outputFile)
 }
---
+++ jcli/genesis_test.go
@@ -0,0 +1,91 @@
+package jcli_test
+
+import (
+	"testing"
+
+	"github.com/rinor/jorcli/jcli"
+)
+
+// TODO: Improve test
+func TestGenesisInit(t *testing.T) {
+	_, err := jcli.GenesisInit()
+	ok(t, err)
+}
+
+func TestGenesisHash_file(t *testing.T) {
+	var (
+		block0Bin          []byte
+		inputFile          = filePath(t, "genesis_block0_bin.golden")
+		expectedBlock0Hash = []byte("480b129ef7e681641adf5b0d849c725ad307f04ab7fe2381905db6f88ca9400f\n")
+	)
+
+	genesisBlock0Hash, err := jcli.GenesisHash(block0Bin, inputFile)
+	ok(t, err)
+	equals(t, expectedBlock0Hash, genesisBlock0Hash) // Prod: bytes.Equal(expectedBlock0Hash, genesisBlock0Hash)
+}
+
+func TestGenesisHash_stdin(t *testing.T) {
+	var (
+		block0Bin          = loadBytes(t, "genesis_block0_bin.golden")
+		inputFile          = ""
+		expectedBlock0Hash = []byte("480b129ef7e681641adf5b0d849c725ad307f04ab7fe2381905db6f88ca9400f\n")
+	)
+
+	genesisBlock0Hash, err := jcli.GenesisHash(block0Bin, inputFile)
+	ok(t, err)
+	equals(t, expectedBlock0Hash, genesisBlock0Hash) // Prod: bytes.Equal(expectedBlock0Hash, genesisBlock0Hash)
+}
+
+func TestGenesisEncode_file(t *testing.T) {
+	var (
+		block0Txt         []byte
+		inputFile         = filePath(t, "genesis_block0_txt.golden")
+		outputFile        = ""
+		expectedBlock0Bin = loadBytes(t, "genesis_block0_bin.golden")
+	)
+
+	genesisBlock0Bin, err := jcli.GenesisEncode(block0Txt, inputFile, outputFile)
+
+	ok(t, err)
+	equals(t, expectedBlock0Bin, genesisBlock0Bin) // Prod: bytes.Equal(expectedBlock0Bin, genesisBlock0Bin)
+}
+
+func TestGenesisEncode_stdin(t *testing.T) {
+	var (
+		block0Txt         = loadBytes(t, "genesis_block0_txt.golden")
+		inputFile         = ""
+		outputFile        = ""
+		expectedBlock0Bin = loadBytes(t, "genesis_block0_bin.golden")
+	)
+
+	genesisBlock0Bin, err := jcli.GenesisEncode(block0Txt, inputFile, outputFile)
+
+	ok(t, err)
+	equals(t, expectedBlock0Bin, genesisBlock0Bin) // Prod: bytes.Equal(expectedBlock0Bin, genesisBlock0Bin)
+}
+
+func TestGenesisDecode_file(t *testing.T) {
+	var (
+		block0Bin         []byte
+		inputFile         = filePath(t, "genesis_block0_bin.golden")
+		outputFile        = ""
+		expectedBlock0Txt = loadBytes(t, "genesis_block0_txt.golden")
+	)
+
+	genesisBlock0Txt, err := jcli.GenesisDecode(block0Bin, inputFile, outputFile)
+	ok(t, err)
+	equals(t, expectedBlock0Txt, genesisBlock0Txt) // Prod: bytes.Equal(expectedBlock0Txt, genesisBlock0Txt)
+}
+
+func TestGenesisDecode_stdin(t *testing.T) {
+	var (
+		block0Bin         = loadBytes(t, "genesis_block0_bin.golden")
+		inputFile         = ""
+		outputFile        = ""
+		expectedBlock0Txt = loadBytes(t, "genesis_block0_txt.golden")
+	)
+
+	genesisBlock0Txt, err := jcli.GenesisDecode(block0Bin, inputFile, outputFile)
+	ok(t, err)
+	equals(t, expectedBlock0Txt, genesisBlock0Txt) // Prod: bytes.Equal(expectedBlock0Txt, genesisBlock0Txt)
+}
