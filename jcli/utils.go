bceb3ca923c2e9ee920d944b208b9971d6d7c939
rinor <rinorhoxha@gmail.com>
2019-08-30 08:26:33 +0000

    JCLI: utils - golint fix, add tests

--- jcli/utils.go
+++ jcli/utils.go
@@ -8,14 +8,14 @@ import (
 // UtilsBech32ConvertFromStdin - convert a bech32 with hrp n into a bech32 with prefix m.
 // jcli does not use STDIN for this function, but keep it for convenience
 func UtilsBech32ConvertFromStdin(
-	stdin_bech32 []byte,
-	new_prefix string,
+	bech32 []byte,
+	newPrefix string,
 ) ([]byte, error) {
-	if len(stdin_bech32) == 0 {
-		return nil, fmt.Errorf("%s : EMPTY", "stdin_bech32")
+	if len(bech32) == 0 {
+		return nil, fmt.Errorf("%s : EMPTY", "bech32")
 	}
 
-	return UtilsBech32Convert(strings.TrimSuffix(string(stdin_bech32), "\n"), new_prefix)
+	return UtilsBech32Convert(strings.TrimSuffix(string(bech32), "\n"), newPrefix)
 }
 
 // UtilsBech32Convert - convert a bech32 with hrp n into a bech32 with prefix m.
@@ -23,16 +23,16 @@ func UtilsBech32ConvertFromStdin(
 // jcli utils bech32-convert <FROM_BECH32> <NEW_PREFIX>
 func UtilsBech32Convert(
 	bech32 string,
-	new_prefix string,
+	newPrefix string,
 ) ([]byte, error) {
 	if bech32 == "" {
 		return nil, fmt.Errorf("parameter missing : %s", "bech32")
 	}
-	if new_prefix == "" {
-		return nil, fmt.Errorf("parameter missing : %s", "new_prefix")
+	if newPrefix == "" {
+		return nil, fmt.Errorf("parameter missing : %s", "newPrefix")
 	}
 
-	arg := []string{"utils", "bech32-convert", bech32, new_prefix}
+	arg := []string{"utils", "bech32-convert", bech32, newPrefix}
 
 	return execStd(nil, "jcli", arg...)
 }
---
+++ jcli/utils_test.go
@@ -0,0 +1,43 @@
+package jcli_test
+
+import (
+	"fmt"
+
+	"github.com/rinor/jorcli/jcli"
+)
+
+func ExampleUtilsBech32Convert() {
+	var (
+		bech32    = "ta1s4uxkxptz3zx7akmugkmt4ecjjd3nmzween2qfr5enhzkt37tdt4ulu8sap"
+		newPrefix = "ca"
+	)
+
+	bech32WithNewPrefix, err := jcli.UtilsBech32Convert(bech32, newPrefix)
+
+	if err != nil {
+		fmt.Printf("UtilsBech32Convert: %s", err)
+	} else {
+		fmt.Printf("%s", string(bech32WithNewPrefix))
+	}
+	// Output:
+	//
+	// ca1s4uxkxptz3zx7akmugkmt4ecjjd3nmzween2qfr5enhzkt37tdt4ugqz89h
+}
+
+func ExampleUtilsBech32ConvertFromStdin() {
+	var (
+		bech32    = []byte("ta1s4uxkxptz3zx7akmugkmt4ecjjd3nmzween2qfr5enhzkt37tdt4ulu8sap")
+		newPrefix = "ca"
+	)
+
+	bech32WithNewPrefix, err := jcli.UtilsBech32ConvertFromStdin(bech32, newPrefix)
+
+	if err != nil {
+		fmt.Printf("UtilsBech32ConvertFromStdin: %s", err)
+	} else {
+		fmt.Printf("%s", string(bech32WithNewPrefix))
+	}
+	// Output:
+	//
+	// ca1s4uxkxptz3zx7akmugkmt4ecjjd3nmzween2qfr5enhzkt37tdt4ugqz89h
+}
